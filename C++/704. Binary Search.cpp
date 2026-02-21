5515e02207aa7c413bafeec9ebc08d4c56763435
Joseph Caraan <118485950+jczy3@users.noreply.github.com>
2026-02-20 20:56:53 -0800

    Update 704. Binary Search.cpp

--- C++/704. Binary Search.cpp
+++ C++/704. Binary Search.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int n = nums.size();
-        int right = nums.size() - 1;
-        int left = 0;
-        for(int i = 0; i < n; i++){
+        int left{};
+        int right{static_cast<int>(nums.size()-1)};
+        while (left <= right)
+        {
             int mid = left + (right - left) / 2;
-            if (nums[mid] == target) return mid;
-            else if (nums[mid] < target) left = mid + 1;
-            else right = mid - 1;
+            if (nums[mid] > target)
+                right = mid - 1;
+            else if (nums[mid] < target)
+                left = mid + 1;
+            else 
+                return mid;
         }
         return -1;
     }
