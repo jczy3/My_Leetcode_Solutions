4007d59b466c3303f002b4840dcc5c4080495e90
Joseph Caraan <118485950+jczy3@users.noreply.github.com>
2026-02-25 18:09:21 -0800

    Create 33. Search in Rotated Sorted Array.cpp

---
+++ C++/33. Search in Rotated Sorted Array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int right {static_cast<int>(nums.size()-1)};
+        int left {};
+
+        while (left <= right)
+        {
+            int mid {left + (right - left) / 2};
+            if (nums[mid] == target)
+                return mid;
+
+            else if (nums[mid] >= nums[left])
+                if (nums[mid] >= target && target >= nums[left])
+                    right = mid - 1;
+                else
+                    left = mid + 1;
+            
+            else
+                if (nums[right] >= target && target >= nums[mid])
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+        }
+        return -1;
+    }
+};
