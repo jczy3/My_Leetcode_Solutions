bf39ec2ba3eccad1a126df6c87e9e3da23d90119
Joseph Caraan <118485950+jczy3@users.noreply.github.com>
2026-08-29 10:42:27 -0500

    Delete python/neetcode150/217. Contains Duplicate.py

--- python/neetcode150/217. Contains Duplicate.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        nums.sort()
-        prev = nums[0]
-        for i in range (1,len(nums)):
-            if nums[i] == prev:
-                return True
-            prev = nums[i]
-        return False
